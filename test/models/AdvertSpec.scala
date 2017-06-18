e909ced867d574ae4e75b7f4cace236b6a86878f
Raj <raj.saxena@thoughtworks.com>
2017-06-18 15:20:28 +0530

    Added test for old car ad json to model

--- test/models/AdvertSpec.scala
+++ test/models/AdvertSpec.scala
@@ -1,6 +1,7 @@
 package models
 
 import models.Advert._
+import org.joda.time.DateTime
 import org.specs2.mutable.Specification
 import play.api.libs.json.{JsError, JsSuccess, Json}
 
@@ -14,7 +15,21 @@ class AdvertSpec extends Specification {
     IsNew -> true
   )
 
+  private val oldCarTitle = "Old car"
+  private val oldCarPrice = 2345
+  private val dateOfRegistration = DateTime.now().minusYears(3)
+  private val oldCarMileage = 12
+  val oldCarAdJson = Json.obj(
+    Title -> oldCarTitle,
+    Fuel -> "Petrol",
+    Price -> oldCarPrice,
+    IsNew -> false,
+    Mileage -> oldCarMileage,
+    FirstRegistration -> dateOfRegistration
+  )
+
   val newCarAd = Advert(None, newCarTitle, Diesel(), newCarPrice, true)
+  val oldCarAd = Advert(None, oldCarTitle, Petrol(), oldCarPrice, false, Some(oldCarMileage), Some(dateOfRegistration))
 
   "Advert" should {
     "validate and create new car advert from json" in {
@@ -27,6 +42,21 @@ class AdvertSpec extends Specification {
         case error: JsError =>
           println(error)
           false
+      }) mustEqual true
+    }
+
+    "validate and create old car advert from json" in {
+      val advertResult = oldCarAdJson.validate[Advert]
+
+      (advertResult match {
+        case JsSuccess(advert, _) => {
+          advert mustEqual oldCarAd
+          true
+        }
+        case error: JsError => {
+          println(error)
+          false
+        }
       }) mustEqual true
     }
   }
